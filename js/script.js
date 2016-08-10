c8238c4c4229cbd32774499a5943bff53b6aa919
pujan789556 <pujan789556@gmail.com>
2016-08-10 11:17:16 +0545

    Bamboo house

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 config = {
-	api: "http://139.59.28.75/ona_export_tool_permanent_toilet/",
-	authUrl: "http://139.59.28.75/ona_export_tool_permanent_toilet/auth.php",
+	api: "http://139.59.28.75/ona_export_tool_bamboo_house/",
+	authUrl: "http://139.59.28.75/ona_export_tool_bamboo_house/auth.php",
 	dataGroups: ["tunnel_house"],
 	surveyStartDate: "2015-10-23"
 };
