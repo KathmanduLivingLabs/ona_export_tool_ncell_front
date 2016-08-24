8298e31050311ab485d1efafaf9fc9d5c6edc760
pujan789556 <pujan789556@gmail.com>
2016-08-24 12:33:44 +0545

    Merged

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 config = {
-    api: "http://139.59.28.75/ona_export_tool_ncell_tunnel_house/",
-	authUrl: "http://139.59.28.75/ona_export_tool_ncell_tunnel_house/auth.php",
+    api: "http://139.59.28.75/ona_export_tool_ncell_toilet/",
+	authUrl: "http://139.59.28.75/ona_export_tool_ncell_toilet/auth.php",
 	dataGroups: ["tunnel_house"],
 	surveyStartDate: "2015-10-23"
 };
